da58e4cb7bde3552a4b8d748e5b6496da3dc33d8
Ahmed Hassan <ahmedrhassan98@gmail.com>
2021-02-28 01:31:44 +0200

    Solve Assignment #1

---
+++ Unit #2/Lesson 3/Assignment #1/1/1.c
@@ -0,0 +1,24 @@
+/*****************************************************
+EX1:
+
+Write C Program to Print a Sentence
+
+I should see the Console as following:
+##########Console-output###
+C Programming
+###########################
+
+******************************************************/
+
+/* Includes library that used in this project  */
+#include <stdio.h>
+
+/* Main program */
+int main(void)
+{
+	/* Print desired statement */
+	printf("C Programming");
+
+	/* Return from main function */
+	return 0;
+}
